63b336073a8237112754720e8f64b2b5ce73b93e
Inyat <124053142+INYAT-gif@users.noreply.github.com>
2024-12-21 15:27:19 +0100

    Update and rename App.js to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
-const App = () => {
-  const [time, setTime] = useState(0);
+const App: React.FC = () => {
+  const [time, setTime] = useState<number>(0);
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setTime(prevTime => prevTime + 1);
+      setTime((prevTime) => prevTime + 1);
     }, 1000);
 
     return () => clearInterval(timer);
